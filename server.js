7de0ea97fb9347ff5b061422eadbcb3ad42d0fb2
fivo <fivofouka@gmail.com>
2022-12-15 04:06:47 +0200

    public folder

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 app.use(express.json());
-app.use(cors({
-    origin: "http://127.0.0.1:5500"
-}))
+app.use(express.static(__dirname + '/public'));
+// const cors = require('cors');
+// app.use(cors({
+//     origin: "http://127.0.0.1:5500"
+// }))
 let summary;
 
 app.get("/", (req, res) => {
-    res.json('hello:bruv');
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 app.get("/summary", (req, res) => {
